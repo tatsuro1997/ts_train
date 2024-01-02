a7270ef5a403605b0f9cfc9b8a5a9a8955db61cd
tatsuro1997 <minagawa@dptr.jp>
2024-01-02 10:37:24 +0900

    add filtering projects

--- src/app.ts
+++ src/app.ts
@@ -117,7 +117,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListner((projects: any[]) => {
-      this.assignProjects = projects;
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+    });
+      this.assignProjects = relevantProjects;
       this.renderProjects();
     })
 
@@ -126,7 +132,8 @@ class ProjectList {
   }
 
   private renderProjects() {
-    const listEl = document.getElementById(`${this.type}-projects-list`);
+    const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const prjItem of this.assignProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
