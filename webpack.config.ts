eafbcdfdab752d76704f41ba6fc9c829053908e4
tatsuro1997 <minagawa@dptr.jp>
2024-01-06 09:25:47 +0900

    update webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname)
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
-  devtool: 'inline-source-map',
   module: {
     rules: [
       {
